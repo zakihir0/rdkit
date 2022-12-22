a71d386deda76331ae42120aa62ecb7155ec3b9b
zakihiro <hiro1231offic.gmail.com>
2022-12-22 09:14:08 +0900

    rdkit

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 # %%
 import pandas as pd
-from rdkit import rdBase, Chem
+import streamlit as st
+from PIL import Image
+
+from rdkit import Chem, rdBase
+from rdkit.Chem import Draw
 
 dataset = pd.read_csv("csv/molecules_with_boiling_point.csv", index_col=0)
 dataset = dataset.reset_index(drop=True)
+
+mols = [Chem.MolFromSmiles(i) for i in dataset['smiles']]
+DRAWing = Draw.MolToGridImage(mols)
+
+st.image(DRAWing)
 # %%
