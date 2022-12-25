4dd0ec3f946e226b1aa9b1474bb4200e09cfaace
zakihiro <hiro1231office@gmail.com>
2022-12-26 02:06:45 +0900

    code

--- test.py
+++ test.py
@@ -5,12 +5,13 @@ from PIL import Image
 
 from rdkit import Chem, rdBase
 from rdkit.Chem import Draw
-
+#%%
 dataset = pd.read_csv("csv/molecules_with_boiling_point.csv", index_col=0)
 dataset = dataset.reset_index(drop=True)
 
-mols = [Chem.MolFromSmiles(i) for i in dataset['smiles']]
-DRAWing = Draw.MolToGridImage(mols)
-
-st.image(DRAWing)
+mols = [Chem.MolFromSmiles(i) for i in dataset['SMILES']]
+Drawing = Draw.MolsToGridImage(mols)
+#%%
+Drawing = Draw.MolsToImage(mols)
+st.image(Drawing)
 # %%
